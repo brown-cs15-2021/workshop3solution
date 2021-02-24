0b46e4a706e1b858c9bf02556e5b441dcbd15fd3
elliotjinoohong <elliot_hong@brown.edu>
2021-02-23 22:10:33 -0500

    removed unnecessary instance variables in cookie

--- workshop3solution/Cookie.java
+++ workshop3solution/Cookie.java
@@ -6,11 +6,6 @@ import javafx.scene.shape.Circle;
 
 public class Cookie {
     private Pane _cookiePane;
-    private Circle _cookieBase;
-    private Circle _chocChip1;
-    private Circle _chocChip2;
-    private Circle _chocChip3;
-    private Circle _chocChip4;
 
     // Cookie Constructor
     public Cookie(Pane cookiePane) {
@@ -21,24 +16,24 @@ public class Cookie {
         this.makeCookieBase();
 
         // add chocolate chips (1-4)
-        _chocChip1 = this.addChocChip(28,40);
-        _chocChip2 = this.addChocChip(-40,8);
-        _chocChip3 = this.addChocChip(12,-2);
-        _chocChip4 = this.addChocChip(-24,-35);
+        Circle chocChip1 = this.addChocChip(28,40);
+        Circle chocChip2 = this.addChocChip(-40,8);
+        Circle chocChip3 = this.addChocChip(12,-2);
+        Circle chocChip4 = this.addChocChip(-24,-35);
         // forget to add to pane and have students catch the mistake!
         // students might ask "what if we added to the pane in addChocChip?"
         //  |-> this should be okay because we are assigning these shapes (line 24-28) by reference.
-        _cookiePane.getChildren().addAll(_chocChip1, _chocChip2, _chocChip3, _chocChip4);
+        _cookiePane.getChildren().addAll(chocChip1, chocChip2, chocChip3, chocChip4);
     }
 
     private void makeCookieBase(){
         // Instantiate cookie base
-        _cookieBase = new Circle();
-        _cookieBase.setFill(Color.BURLYWOOD);
-        _cookieBase.setCenterX(Constants.COOKIE_POSITION);
-        _cookieBase.setCenterY(Constants.COOKIE_POSITION);
-        _cookieBase.setRadius(Constants.COOKIE_RADIUS);
-        _cookiePane.getChildren().add(_cookieBase);
+        Circle cookieBase = new Circle();
+        cookieBase.setFill(Color.BURLYWOOD);
+        cookieBase.setCenterX(Constants.COOKIE_POSITION);
+        cookieBase.setCenterY(Constants.COOKIE_POSITION);
+        cookieBase.setRadius(Constants.COOKIE_RADIUS);
+        _cookiePane.getChildren().add(cookieBase);
     }
 
     private Circle addChocChip(double x_pos, double y_pos){
